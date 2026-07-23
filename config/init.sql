449c676ea41f7eadc23815a401cc04f868871a63
itaru2622 <itaru2622@gmail.com>
2026-07-23 14:48:48 +0900

    update: docker-compose.yaml   integrates with reverse-proxy(caddy) toward sample in real.

---
+++ config/init.sql
@@ -0,0 +1,10 @@
+-- admin user=postgres
+
+-- logto
+CREATE DATABASE logto;
+GRANT ALL PRIVILEGES ON DATABASE logto TO postgres;
+
+-- another DB
+-- CREATE DATABASE others;
+-- GRANT ALL PRIVILEGES ON DATABASE others TO postgres;
+
